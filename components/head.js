97970225da1bc017a130e459df4351f6fde6b96e
jawn <62079777+jawndiego@users.noreply.github.com>
2021-07-25 22:06:41 -0400

    Update head.js
    'descriptions' delete

--- components/head.js
+++ components/head.js
@@ -10,15 +10,12 @@ const Head = (props) => (
     <meta charSet="UTF-8" />
     <title>{`${props.title} | Jawn  ` || ''}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content={props.description || defaultDescription} />
-    <meta name="keywords" content={props.keywords || defaultKeywords} />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
     <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
     <link rel="shortcut icon" href="/favicons/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
     <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:title" content={props.title || ''} />
-    <meta property="og:description" content={props.description || defaultDescription} />
     <meta name="twitter:site" content={props.url || defaultOGURL} />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
