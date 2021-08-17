e13c92b4f10c31961d45066871e79df88b15ded9
jawn <62079777+jawndiego@users.noreply.github.com>
2021-08-17 03:33:58 -0400

    Added Mint Functionality
    Is able to use 'UseContractFunction' to call MintBase on JAWN contract
    requires:
    -mint button to be pressed
    -and to specify an address + URI

--- components/JawnSidebar.js
+++ components/JawnSidebar.js
@@ -10,15 +10,35 @@ import { Contract } from '@ethersproject/contracts'
 const JawnSidebar = () => {
 
 const { activateBrowserWallet, account } = useEthers()
+// connected wallet information regarding ETH, JAWN (721), and LUPE(20) balances
 const etherBalance = useEtherBalance(account)
-// address is 
 const LUPE = '0xb48497dabaffe085801c1b063e7e54f50b833784'
 const JAWN = '0xf33Cc6f3A9ac3A702aBE0e7070e22644CbdE6826'
 const tokenBalanceLUPE = useTokenBalance(LUPE, account)
 const tokenBalanceJAWN = useTokenBalance(JAWN, account)
+// ask for mint inputs 
+const [to, setTo] = useState('')
+const [uri, setURI] = useState('')
+// Creating a new Contract
+const mintbaseInterface = new utils.Interface(["function mintBase(address to, string uri) returns (uint256)"])
+const contract = new Contract(JAWN, mintbaseInterface)
+// mint 
+const { send } = useContractFunction(contract, 'mintBase')
 
-  return (
-   
+// disables mint button
+const [busy, setBusy] = useState(false)
+
+async function triggerMint() {
+  setBusy(true)
+  if (to && uri) {
+    return (
+      send(to, uri)
+    )
+  }
+  setBusy(false)
+}
+
+  return (   
 
     <div className="doge-sidebar_wrapper">
       
@@ -32,11 +52,15 @@ const tokenBalanceJAWN = useTokenBalance(JAWN, account)
           <div>
       <div>
       </div>
-      <a className="lozenge-button doge-sidebar_history-button" onClick={() => activateBrowserWallet()}>Mint</a>
+      <a className="lozenge-button doge-sidebar_history-button" onClick={triggerMint} disabled={busy}>Mint</a>
       {account && <p>Account: {account}</p>}
       {etherBalance && <p>Balance: {formatEther(etherBalance)} ETH</p>}
       {tokenBalanceJAWN && <p>Balance: {formatUnits(tokenBalanceJAWN, 0)} JAWN</p>}
       {tokenBalanceLUPE && <p>Balance: {formatUnits(tokenBalanceLUPE, 18)} LUPE</p>}
+      <p>to Who?</p>
+      <input value={to} onChange={event => setTo(event.target.value)}/>
+      <p>enterURI:</p>
+      <input value={uri} onChange={event => setURI(event.target.value)}/>
     
      
       
