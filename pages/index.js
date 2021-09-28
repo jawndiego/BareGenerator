2d5b11f602e9bf147b5d1413903ecf056acbf958
Jonathan Reis <jonathanreis@Jonathans-MBP.lan>
2021-09-28 19:13:07 -0400

    some styling

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Index({ data }) {
       <Head
         title="ALGO LITE"
       />
+    <img className="name-text" src="name-text.png" />
       <JawnSidebar showMint={true} showHome={false} showFaq={true} showConnect={true} />
       <FrontArt />
     </DAppProvider>
