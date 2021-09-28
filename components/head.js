1e0e591025ecd66d121a8c2bf6f377bb72d63c05
Jonathan Reis <jonathanreis@Jonathans-MBP.lan>
2021-09-28 13:49:27 -0400

    favicon

--- components/head.js
+++ components/head.js
@@ -10,10 +10,11 @@ const Head = (props) => (
     <meta charSet="UTF-8" />
     <title>{`${props.title} | Jawn  ` || ''}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
+    <link rel="icon" type="image/png" href="/favicons/favicon.png" />
+    {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
     <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
     <link rel="shortcut icon" href="/favicons/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" /> */}
     <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:title" content={props.title || ''} />
     <meta name="twitter:site" content={props.url || defaultOGURL} />
