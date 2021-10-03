f8d019fc613d6b4337fb1b4515284e6b561c7a4c
Jonathan Reis <jonathanreis@Jonathans-MacBook-Pro.local>
2021-10-03 00:40:56 -0400

    use walletConnect if mobile

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,29 @@ import FrontArt from '../components/frontart'
 import JawnSidebar from '../components/JawnSidebar'
 import { ChainId, DAppProvider } from '@usedapp/core'
 
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+
 const config = {
   readOnlyChainId: ChainId.Rinkeby,
   readOnlyUrls: {
     [ChainId.Mainnet]: 'https://rinkeby.infura.io/v3/21acab1ef4fb416387f7391d15f5e0f7',
   },
 }
+
+const walletConnect = new WalletConnectConnector({
+  rpc: config,
+  qrcode: true,
+  qrcodeModalOptions: {
+    mobileLinks: [
+      "rainbow",
+      "metamask",
+      "argent",
+      "trust",
+      "imtoken",
+      "pillar",
+    ],
+  },
+})
 
 export default function Index({ data }) {
   return (
@@ -17,7 +34,7 @@ export default function Index({ data }) {
         title="ALGO LITE"
       />
     <img className="name-text" src="name-text.png" />
-      <JawnSidebar showMint={true} showHome={false} showFaq={true} showConnect={true} />
+      <JawnSidebar walletConnect={walletConnect} showMint={true} showHome={false} showFaq={true} showConnect={true} />
       <FrontArt />
     </DAppProvider>
   )
